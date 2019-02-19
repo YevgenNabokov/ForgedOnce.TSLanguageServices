d74a59fd06f54250ce2123ec9d2e64b36207124f
Yevgen <55064156+YevgenNabokov@users.noreply.github.com>
2019-02-20 00:07:11 +0100

    Added design sketches. Building syntax tree for CS to TS translator.

---
+++ Game08.Sdk.CSToTS.Translator.UnitTests/SomeTests.cs
@@ -0,0 +1,32 @@
+﻿using Game08.Sdk.CSToTS.Translator;
+using NUnit.Framework;
+using System;
+
+namespace Game08.Sdk.CSToTS.UnitTests
+{
+    [TestFixture]
+    public class SomeTests
+    {
+        [Test]
+        public void ParserTest()
+        {
+            var sampleCode = @"using System;
+            namespace MyNamespace
+            {
+                public class MyAwesomeClass
+                {
+                    public int MyMethod(int a, int b)
+                    {
+                        return a + b;
+                    }
+                }
+            }";
+
+            CodeTranslator subject = new CodeTranslator();
+
+            subject.AddPage("input", sampleCode, "output");
+
+            var result = subject.Translate();
+        }
+    }
+}
