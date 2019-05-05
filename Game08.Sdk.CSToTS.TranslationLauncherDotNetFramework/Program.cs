e5bf9c515ad02f15644d8d32c3ab2ecbbada4780
Yevgen <55064156+YevgenNabokov@users.noreply.github.com>
2019-05-05 15:50:12 +0200

    Added single project generation capability.

--- Game08.Sdk.CSToTS.TranslationLauncherDotNetFramework/Program.cs
+++ Game08.Sdk.CSToTS.TranslationLauncherDotNetFramework/Program.cs
@@ -5,13 +5,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Game08.Sdk.CSToTS.TranslationLauncherDotNetFramework
+namespace Game08.Sdk.CSToTS.TranslationLauncher
 {
     class Program
     {
         static void Main(string[] args)
         {
-            TranslationManager manager = new TranslationManager(args[0], args[1]);
+            TranslationManager manager = new TranslationManager(args[0], args[1], args.Length > 2 ? new string[] { args[2] } : null);
 
             manager.Translate();
         }
--- Game08.Sdk.CSToTS.TranslationLauncherDotNetFramework/Properties/AssemblyInfo.cs
+++ Game08.Sdk.CSToTS.TranslationLauncherDotNetFramework/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Game08.Sdk.CSToTS.TranslationLauncherDotNetFramework")]
+[assembly: AssemblyTitle("Game08.Sdk.CSToTS.TranslationLauncher")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Game08.Sdk.CSToTS.TranslationLauncherDotNetFramework")]
+[assembly: AssemblyProduct("Game08.Sdk.CSToTS.TranslationLauncher")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
