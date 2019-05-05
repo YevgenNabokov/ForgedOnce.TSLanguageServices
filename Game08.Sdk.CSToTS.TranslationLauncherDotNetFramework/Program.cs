86d657dd165e091fb3654827a1feb6a3a6bb7657
Yevgen <55064156+YevgenNabokov@users.noreply.github.com>
2019-05-05 02:11:32 +0200

    Added net472 targeted launcher to avoid issues with multiproject solution.

---
+++ Game08.Sdk.CSToTS.TranslationLauncherDotNetFramework/Program.cs
@@ -0,0 +1,19 @@
+﻿using Game08.Sdk.CSToTS.TranslationLauncher;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game08.Sdk.CSToTS.TranslationLauncherDotNetFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TranslationManager manager = new TranslationManager(args[0], args[1]);
+
+            manager.Translate();
+        }
+    }
+}
