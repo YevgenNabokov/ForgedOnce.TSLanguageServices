2b376a026f01cbf1a083d57f86c6ab3544a71400
Yevgen <55064156+YevgenNabokov@users.noreply.github.com>
2019-06-08 13:36:12 +0200

    Backed out changeset: c6933c5bd544

--- Game08.Sdk.CSToTS.TypeScriptBuilder/TestFile.ts
+++ Game08.Sdk.CSToTS.TypeScriptBuilder/TestFile.ts
@@ -15,21 +15,6 @@ export class BaseC<T> {
     }
 }
 
-export interface TI {
-    A: number;
-}
-
-export class TC implements TI {
-    private _a: number;
-
-    get A(): number {
-        return this._a;
-    }
-    set A(value: number) {
-        this._a = value;
-    }
-}
-
 export class TestC<A> extends otf.OC<A> {
     private _s: number;
 
