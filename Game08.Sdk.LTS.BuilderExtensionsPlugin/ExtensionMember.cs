9fbb24691e05fc106467efd6fb4a138245759e5e
Yevgen <55064156+YevgenNabokov@users.noreply.github.com>
2019-12-15 23:19:49 +0100

    Implementing extensions generator.

---
+++ Game08.Sdk.LTS.BuilderExtensionsPlugin/ExtensionMember.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game08.Sdk.LTS.BuilderExtensionsPlugin
+{
+    public class ExtensionMember
+    {
+        public string Name;
+
+        public IPropertySymbol SourcePropertySymbol;
+
+        public bool IsCollection;
+
+        public ITypeSymbol ItemType;
+    }
+}
--- Game08.Sdk.LTS.BuilderExtensionsPlugin/Plugin.cs
+++ Game08.Sdk.LTS.BuilderExtensionsPlugin/Plugin.cs
@@ -3,8 +3,12 @@ using Game08.Sdk.CodeMixer.Core.Interfaces;
 using Game08.Sdk.CodeMixer.Core.Metadata.Interfaces;
 using Game08.Sdk.CodeMixer.Core.Plugins;
 using Game08.Sdk.CodeMixer.CSharp;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Game08.Sdk.LTS.BuilderExtensionsPlugin
@@ -35,6 +39,144 @@ namespace Game08.Sdk.LTS.BuilderExtensionsPlugin
 
         protected override void Implementation(CodeFileCSharp input, Parameters parameters, IMetadataRecorder metadataRecorder, ILogger logger)
         {
+            foreach (var classDeclaration in input.SyntaxTree.GetRoot().DescendantNodes().OfType<ClassDeclarationSyntax>())
+            {
+                if (!string.IsNullOrEmpty(this.Settings.RequiredClassBaseType))
+                {
+                    INamedTypeSymbol requiredType = input.SemanticModel.Compilation.GetTypeByMetadataName(this.Settings.RequiredClassBaseType);
+                    if (requiredType == null)
+                    {
+                        throw new InvalidOperationException($"Cannot resolve required base type {this.Settings.RequiredClassBaseType} from compilation.");
+                    }
+
+                    bool hasRequiredType = false;
+                    foreach (var baseType in classDeclaration.BaseList.Types)
+                    {
+                        var typeInfo = input.SemanticModel.GetTypeInfo(baseType.Type);
+
+                        if (InheritsFromOrImplementsOrEqualsIgnoringConstruction(typeInfo.Type, requiredType))
+                        {
+                            hasRequiredType = true;
+                            break;
+                        }
+                    }
+
+                    if (!hasRequiredType)
+                    {
+                        continue;
+                    }
+                }
+
+                var declaredSymbol = input.SemanticModel.GetDeclaredSymbol(classDeclaration);
+                var outputFile = (CodeFileCSharp)this.outputStream.CreateCodeFile($"{declaredSymbol.Name}Extensions.cs");
+
+                this.GenerateForClass(input, classDeclaration, declaredSymbol, outputFile);
+            }
+        }
+
+        private void GenerateForClass(CodeFileCSharp input, ClassDeclarationSyntax classDeclarationSyntax, INamedTypeSymbol declaredSymbol, CodeFileCSharp output)
+        {
+            var unit = SyntaxFactory.CompilationUnit();
+            
+            unit = unit.AddUsings(SyntaxFactory.UsingDirective(SyntaxFactory.IdentifierName(declaredSymbol.ContainingNamespace.ToDisplayString())));
+
+            foreach (var nsUsage in input.SyntaxTree.GetRoot().DescendantNodes().OfType<UsingDirectiveSyntax>())
+            {
+                unit = unit.AddUsings(nsUsage);
+            }
+
+            var props = this.GetAllPublicProperties(declaredSymbol);
+
+            var additionalUsings = new HashSet<string>();
+
+            List<ExtensionMember> members = new List<ExtensionMember>();
+
+            foreach (var p in props)
+            {
+                var itemType = p.Type;
+                bool isCollection = false;
+                var collectionInterface = p.Type.Interfaces.FirstOrDefault(i => i.MetadataName == typeof(ICollection<>).FullName);
+                
+                if (collectionInterface != null)
+                {
+                    itemType = collectionInterface.TypeArguments.First();
+                    isCollection = true;
+                    additionalUsings.Add(typeof(ICollection<>).FullName.Substring(0, typeof(ICollection<>).FullName.LastIndexOf('.')));
+                }
+
+                if (itemType.ContainingNamespace.ToDisplayString() != declaredSymbol.ContainingNamespace.ToDisplayString())
+                {
+                    additionalUsings.Add(itemType.ContainingNamespace.ToDisplayString());
+                }
+
+                members.Add(new ExtensionMember()
+                {
+                    IsCollection = isCollection,
+                    ItemType = itemType,
+                    Name = p.Name,
+                    SourcePropertySymbol = p
+                });
+            }
+
+            foreach (var import in additionalUsings)
+            {
+                unit = unit.AddUsings(SyntaxFactory.UsingDirective(SyntaxFactory.IdentifierName(import)));
+            }
+
+            var className = $"{declaredSymbol.Name}Extensions";
+
+            throw new NotImplementedException();
+        }
+
+        private List<IPropertySymbol> GetAllPublicProperties(INamedTypeSymbol symbol)
+        {
+            var result = new Dictionary<string, IPropertySymbol>();
+
+            foreach (var p in symbol
+                .GetMembers()
+                .Where(m => m.DeclaredAccessibility == Accessibility.Public && !m.IsStatic && m.Kind == SymbolKind.Property)
+                .OfType<IPropertySymbol>())
+            {
+                result.Add(p.Name, p);
+            }
+
+            if (symbol.BaseType != null)
+            {
+                foreach (var p in this.GetAllPublicProperties(symbol.BaseType))
+                {
+                    if (!result.ContainsKey(p.Name))
+                    {
+                        result.Add(p.Name, p);
+                    }
+                }
+            }
+
+            return result.Values.ToList();
+        }
+
+        public static bool InheritsFromOrImplementsOrEqualsIgnoringConstruction(
+            ITypeSymbol type, ITypeSymbol baseType)
+        {
+            var originalBaseType = baseType.OriginalDefinition;
+            type = type.OriginalDefinition;
+
+            if (type.MetadataName == originalBaseType.MetadataName)
+            {
+                return true;
+            }
+
+            IEnumerable<ITypeSymbol> baseTypes = (baseType.TypeKind == TypeKind.Interface) ? type.AllInterfaces : GetBaseTypes(type);
+            return baseTypes.Any(t => t.OriginalDefinition.MetadataName == originalBaseType.MetadataName);
+        }
+
+        public static IEnumerable<INamedTypeSymbol> GetBaseTypes(ITypeSymbol type)
+        {
+            var current = type.BaseType;
+            while (current != null)
+            {
+                yield return current;
+                current = current.BaseType;
+            }
         }
     }
 }
