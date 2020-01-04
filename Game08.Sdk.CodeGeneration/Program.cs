6c55aee34d54ec717f9e4d0276843d133ce4926b
Yevgen <55064156+YevgenNabokov@users.noreply.github.com>
2020-01-04 01:10:35 +0100

    Found a way to debug plugins in consumer project. Added separate build configuration for CLI tools.

---
+++ Game08.Sdk.CodeGeneration/Program.cs
@@ -0,0 +1,16 @@
+﻿using Game08.Sdk.CodeMixer.Launcher.MSBuild.WithDefaultAdapters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game08.Sdk.CodeGeneration
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Launcher launcher = new Launcher();
+            launcher.Launch(args[0], args[1]);
+        }
+    }
+}
